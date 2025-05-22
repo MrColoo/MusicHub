816d78b9984d3e1628542328e94504a93118ac9a
Borto <mattiaborto71@gmail.com>
2025-05-22 10:32:54 +0200

    Funzione di login modificata, in attesa della funzione showEsplora()

--- src/main/java/com/musicsheetsmanager/controller/RegisterController.java
+++ src/main/java/com/musicsheetsmanager/controller/RegisterController.java
@@ -6,14 +6,9 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.musicsheetsmanager.model.Utente;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,30 +18,34 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// Controller per la registrazione di nuovi utenti
 public class RegisterController {
 
     @FXML
-    private TextField registerEmailField;
+    private TextField registerEmailField; // input per EMAIL
     @FXML
-    private TextField registerUsernameField;
+    private TextField registerUsernameField; // input per USERNAME
     @FXML
-    private TextField registerPasswordField;
+    private TextField registerPasswordField; // input per PASSWORD
     @FXML
-    private Button registerButton;
+    private Button registerButton; // bottone per la registrazione
     @FXML
-    private Text feedbackText;
+    private Text feedbackText; // testo di feedback(comunicare verso l'utente se manca qualcosa)
 
-    private static final Path USER_JSON_PATH = Paths.get(
+    private static final Path USER_JSON_PATH = Paths.get( // percorso verso il file JSON
             "src", "main", "resources",
             "com", "musicsheetsmanager", "data", "user.json"
     );
 
     @FXML
-    private void onRegisterClicked() {
+    private void onRegisterClicked() { // prendi i valori dei campi, rimuove tramite trim() spazi iniziali e finali
         String email = registerEmailField.getText().trim();
         String username = registerUsernameField.getText().trim();
         String password = registerPasswordField.getText().trim();
 
+
+        // se un campo è vuoto mostra il messaggio di errore tramite il feedbackText
         if (email.isEmpty()) {
             feedbackText.setText("Inserisci la tua mail!");
             return;
@@ -62,17 +61,19 @@ public class RegisterController {
             return;
         }
 
-        Utente newUser = new Utente(email, username, password);
+        // crea un nuovo utente con i dati inseriti
+        Utente newUser = new Utente(email, username, password); // nuovo utente che si registra
         salvaUtenteInJson(newUser);
 
+        // Mostro la conferma di registrazione
         feedbackText.setText("Registrazione avvenuta con successo!");
 
         // Vai alla schermata di login
-        cambiaASchermataLogin();
+        show("Login");
     }
 
     private void salvaUtenteInJson(Utente newUser) {
-        // Crea un Gson con pretty printing
+        // Crea un Gson con la funzione di pretty printing (formattandolo in maniera leggibile e con nuove linee)
         Gson gson = new GsonBuilder()
                 .setPrettyPrinting()
                 .create();
@@ -98,18 +99,6 @@ public class RegisterController {
             gson.toJson(users, writer);
             System.out.println("Utenti salvati correttamente: totale = " + users.size());
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void cambiaASchermataLogin() {
-        try {
-            Parent root = FXMLLoader.load(getClass()
-                    .getResource("/com/musicsheetsmanager/view/LoginView.fxml"));
-            Stage stage = (Stage) registerButton.getScene().getWindow();
-            stage.setScene(new Scene(root));
-        } catch (IOException e) {
-            System.err.println("Errore nel caricamento della schermata di login:");
             e.printStackTrace();
         }
     }
