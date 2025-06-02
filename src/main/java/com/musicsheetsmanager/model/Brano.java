0c00bcc2557a632e83cde09b3b5a86fcb04150d1
Pande <172507286+SenPande@users.noreply.github.com>
2025-06-02 15:56:43 +0200

    Spostato funzione cerca brano nella classe Brano

--- src/main/java/com/musicsheetsmanager/model/Brano.java
+++ src/main/java/com/musicsheetsmanager/model/Brano.java
@@ -2,6 +2,8 @@ package com.musicsheetsmanager.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class Brano {
     private String titolo;
@@ -11,14 +13,16 @@ public class Brano {
     private List<String> esecutori;
     private List<String> strumentiMusicali;
     private String linkYoutube;
+    private String idBrano;
 
 
     private List<Documento> documenti;
-    private List<Commento> commenti;
+    private List<String> idCommenti;
 
     public Brano(){};
 
     public Brano(String titolo, List<String> autori, String genere, Integer annoComposizione, String linkYoutube, List<String> strumentiMusicali) {
+        this.idBrano = UUID.randomUUID().toString();     // genera id alfanumerico casuale
         this.titolo = titolo;
         this.autori = autori;
         this.genere = genere;
@@ -27,7 +31,7 @@ public class Brano {
         this.strumentiMusicali = new ArrayList<>();
 
         this.documenti = new ArrayList<>();
-        this.commenti = new ArrayList<>();
+        this.idCommenti = new ArrayList<>();
     }
 
     // Getter e Setter
@@ -56,8 +60,12 @@ public class Brano {
         return documenti;
     }
 
-    public List<Commento> getCommenti() {
-        return commenti;
+    public List<String> getIdCommenti() {
+        return idCommenti;
+    }
+
+    public String getIdBrano() {
+        return idBrano;
     }
 
     public String toString() {
@@ -67,5 +75,27 @@ public class Brano {
                 " (" +
                 annoComposizione +
                 ")";
+    }
+
+    // ricerca per titolo-autori-esecutori
+    public static List<Brano> cerca (List<Brano> brani, String chiave){
+
+        if(chiave == null || chiave.isBlank()) return brani;
+
+        String key = chiave.toLowerCase();
+        return brani.stream()
+                .filter(b ->
+                        (b.getTitolo() != null && b.getTitolo().toLowerCase().contains(key)) ||
+                                (b.getAutori() != null && b.getAutori().stream()
+                                        .anyMatch(e -> e.toLowerCase().contains(key))) ||
+                                (b.getEsecutori() != null && b.getEsecutori().stream()
+                                        .anyMatch(e -> e.toLowerCase().contains(key)))
+                )
+                .collect(Collectors.toList());
+    }
+
+    public void aggiungiCommento(String idCommento) {
+        if (idCommenti == null) idCommenti = new ArrayList<>();
+        idCommenti.add(idCommento);
     }
 }
