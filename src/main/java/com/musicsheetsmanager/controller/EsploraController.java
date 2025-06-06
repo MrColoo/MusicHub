a927e61f937fadafe7ff03b66faba907e6ec7daa
Pande <172507286+SenPande@users.noreply.github.com>
2025-06-06 16:15:55 +0200

    Aggiunta funzione per cambiare scena quando si clicca su un brano

--- src/main/java/com/musicsheetsmanager/controller/EsploraController.java
+++ src/main/java/com/musicsheetsmanager/controller/EsploraController.java
@@ -1,5 +1,7 @@
 package com.musicsheetsmanager.controller;
 
+import com.google.gson.reflect.TypeToken;
+import com.musicsheetsmanager.config.JsonUtils;
 import com.musicsheetsmanager.model.Brano;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -11,22 +13,53 @@ import javafx.scene.text.Text;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 
+import javafx.scene.input.MouseEvent;
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class EsploraController{
+public class EsploraController implements  Controller{
     @FXML private FlowPane braniContainer;
+
+    private MainController mainController;
+
+    @Override
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    private List<Brano> listaBraniPota;
+    private static final Path BRANI_JSON_PATH = Paths.get( // percorso verso il file JSON
+            "src", "main", "resources",
+            "com", "musicsheetsmanager", "data", "brani.json"
+    );
+    public void initialize() {
+        Type branoType = new TypeToken<List<Brano>>() {}.getType();
+        listaBraniPota = JsonUtils.leggiDaJson(BRANI_JSON_PATH, branoType);
+        mostraBraniPota(listaBraniPota);
+    }
+
+    public void mostraBraniPota(List<Brano> listaBrani) {
+         braniContainer.getChildren().clear(); // pulisce view precedente
+
+         for(Brano brano: listaBrani) {
+             VBox card = creaCardBrano(brano, brano.getIdBrano());
+             braniContainer.getChildren().add(card);
+         }
+     }
 
     public void mostraBrani(List<Brano> listaBrani) {
         braniContainer.getChildren().clear(); // pulisce view precedente
 
         for(Brano brano: listaBrani) {
-            VBox card = creaCardBrano(brano);
+            VBox card = creaCardBrano(brano, brano.getIdBrano());
             braniContainer.getChildren().add(card);
         }
     }
 
-    private VBox creaCardBrano(Brano brano) {
+    private VBox creaCardBrano(Brano brano, String idBrano) {
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
         vbox.setMaxWidth(154.0);
@@ -34,12 +67,12 @@ public class EsploraController{
         vbox.setPadding(new Insets(15));
         vbox.setCursor(Cursor.HAND);
 
-        ImageView cover = new ImageView(new Image(getClass().getResourceAsStream("/com/musicsheetsmanager/ui/covers/alfa.jpg")));
+        ImageView cover = new ImageView(new Image(getClass().getResourceAsStream("/com/musicsheetsmanager/ui/covers/" + idBrano + ".jpg")));
         cover.setFitWidth(154);
         cover.setPreserveRatio(true);
         cover.setPickOnBounds(true);
 
-        Text titolo = new Text(brano.getTitolo()); // Assicurati che esista getTitolo()
+        Text titolo = new Text(brano.getTitolo());
         titolo.getStyleClass().addAll("text-white", "font-bold", "text-base");
         VBox.setMargin(titolo, new Insets(7, 0, 0, 0));
 
@@ -51,4 +84,8 @@ public class EsploraController{
         return vbox;
     }
 
+    @FXML
+    private void onBranoClick(MouseEvent event) {
+        mainController.show("Brano");
+    }
 }
