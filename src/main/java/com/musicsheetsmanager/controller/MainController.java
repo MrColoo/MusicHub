dd97da049c8202c689ee40fb410efb513dafbd0b
Pande <172507286+SenPande@users.noreply.github.com>
2025-06-24 15:17:54 +0200

    Fix goToConcerto

--- src/main/java/com/musicsheetsmanager/controller/MainController.java
+++ src/main/java/com/musicsheetsmanager/controller/MainController.java
@@ -117,9 +117,7 @@ public class MainController {
 
     // vai alla pagina "Concerto"
     public void goToConcerto(Node node, Concerto concerto, Runnable onPageReady) {
-        if (node != null) {
-            node.setUserData(concerto);
-        }
+        node.setUserData(concerto);
 
         show("Concerto");
         Platform.runLater(onPageReady);
