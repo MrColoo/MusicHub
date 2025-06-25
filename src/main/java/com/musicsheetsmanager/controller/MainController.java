3ea8dacbba767c76ffde27126fe28971a128a718
MrColo <colmat967@gmail.com>
2025-06-25 16:54:55 +0200

    Finita grafica esplora concerti

--- src/main/java/com/musicsheetsmanager/controller/MainController.java
+++ src/main/java/com/musicsheetsmanager/controller/MainController.java
@@ -140,7 +140,7 @@ public class MainController {
 
     // vai alla pagina "Concerto"
     public void goToConcerto(Node node, Concerto concerto, Runnable onPageReady) {
-        node.setUserData(concerto);
+        if(node != null) node.setUserData(concerto);
 
         show("Concerto");
         Platform.runLater(onPageReady);
